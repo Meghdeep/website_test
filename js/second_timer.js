7ba057c59eeb9c55576ed5ca6ac1568c9504c798
Meghdeep <meghdeepr@gmail.com>
2015-12-18 04:59:11 +0530

    Added Credits Page and Linking

--- js/second_timer.js
+++ js/second_timer.js
@@ -20,6 +20,7 @@ function check()
 function execute()
 {
     clearTimeout(id);
+    document.getElementById("credits_div").style.display = "block"
     document.getElementById("header1").innerHTML = "Happy Birthday Sreya !!!";
     document.getElementById("header1").className = "header1";
     document.getElementById("header2").innerHTML = "Happy Birthday and Many Happy Returns of the Day ! Have a LEGEND... Wait for it ... DARY 22nd !!! :D";
