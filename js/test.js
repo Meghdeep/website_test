57f9b0a83bb34576ebac8bef3debc06b249f565a
Meghdeep <meghdeepr@gmail.com>
2015-12-18 03:16:13 +0530

    Final Changes Completed, Countdown changes into Video. Only thing left is to add the video in

--- js/test.js
+++ js/test.js
@@ -1,22 +1,22 @@
 window.onload = function() 
 {
     alert("hi");
-    document.getElementById("Content").innerHTML = "
-    setTimeout(check,3000);
-}
-
-function check()
-{   
-    alert("hi");
-    document.getElementById("Content").innerHTML = "";
-}
-
-function execute()
-{
-    clearTimeout(id);
-    document.getElementById("header1").innerHTML = "Happy Birthday Sreya !!!";
-    document.getElementById("header1").className = "header1";
-    document.getElementById("header2").innerHTML = "Happy Birthday and Many Happy Returns of the Day ! Have a LEGEND... Wait for it ... DARY 22nd !!! :D";
+    parent = document.getElementById("Content");
+    child = document.getElementById("countdown_div");
     
-    //document.getElementById("header1").innerHTML = "Happy Birthday Sreya !!!";
+    //<iframe width="1280" align="middle" height="720" src="http://www.youtube.com/embed/XGSy3_Czz8k?autoplay=1">
+    //</iframe> 
+    var link = "http://www.youtube.com/embed/XGSy3_Czz8k?autoplay=1"    // Put the Link to the Video Here !!!
+    var iframe = document.createElement('iframe');
+    iframe.frameBorder=0;
+    iframe.width="480px";
+    iframe.height="360px";
+    //iframe.className="centered-thing";
+    iframe.id="randomid";
+    
+    iframe.margin="auto";
+    iframe.position="absolute";
+    
+    iframe.setAttribute("src", link);
+    parent.replaceChild(iframe, child);
 }
